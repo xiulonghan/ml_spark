e6aafbb2afb1401a78673eb769f7334734252573
hanxiulong <aluka_han@163.com>
2019-07-31 15:19:57 +0800

    feat:调整代码pep8规范

--- src/dataset_split.py
+++ src/dataset_split.py
@@ -26,35 +26,56 @@ def read_data(file_path, sep=',', app_name='split_dataset',
     :return:
     """
     spark = SparkSession\
-            .builder\
-            .appName(app_name)\
-            .master(master)\
-            .getOrCreate()
+        .builder\
+        .appName(app_name)\
+        .master(master)\
+        .getOrCreate()
     sample_df = spark.read.csv(file_path, sep=sep, header=True)
     # 正负样本随机切割
     # 基于随机数种子进行正负样本的切割
     # 转换为带index的rdd
     sample_rdd = sample_df.rdd.zipWithIndex()
-    pos_index = sample_rdd.filter(lambda x: int(x[0]['label']) == 1).map(lambda x: x[1])
-    neg_index = sample_rdd.filter(lambda x: int(x[0]['label']) == 0).map(lambda x: x[1])
+    pos_index = sample_rdd.filter(
+        lambda x: int(
+            x[0]['label']) == 1).map(
+        lambda x: x[1])
+    neg_index = sample_rdd.filter(
+        lambda x: int(
+            x[0]['label']) == 0).map(
+        lambda x: x[1])
     # 筛选
     random.seed(random_state)
-    pos_test_index = random.sample(pos_index.collect(), int(pos_index.count()*test_size))
-    pos_train_index = list(np.setdiff1d(np.array(pos_index.collect()), np.array(pos_test_index)))
+    pos_test_index = random.sample(
+        pos_index.collect(), int(
+            pos_index.count() * test_size))
+    pos_train_index = list(
+        np.setdiff1d(
+            np.array(
+                pos_index.collect()),
+            np.array(pos_test_index)))
 
-    neg_test_index = random.sample(neg_index.collect(), int(neg_index.count()*test_size))
-    neg_train_index = list(np.setdiff1d(np.array(neg_index.collect()), np.array(neg_test_index)))
+    neg_test_index = random.sample(
+        neg_index.collect(), int(
+            neg_index.count() * test_size))
+    neg_train_index = list(
+        np.setdiff1d(
+            np.array(
+                neg_index.collect()),
+            np.array(neg_test_index)))
     pos_test_index.extend(neg_test_index)
     pos_train_index.extend(neg_train_index)
 
-    test_sample = sample_rdd.filter(lambda x: x[1] in pos_test_index).map(lambda x: x[0])
-    train_sample = sample_rdd.filter(lambda x: x[1] in pos_train_index).map(lambda x: x[0])
+    test_sample = sample_rdd.filter(
+        lambda x: x[1] in pos_test_index).map(
+        lambda x: x[0])
+    train_sample = sample_rdd.filter(
+        lambda x: x[1] in pos_train_index).map(
+        lambda x: x[0])
     return test_sample, train_sample
 
 
 if __name__ == "__main__":
     path = os.path.abspath('..')
-    file_path = path+'//data//test.csv'
+    file_path = path + '//data//test.csv'
     test, train = read_data(file_path, sep='\t')
     print(test.take(10))
-
